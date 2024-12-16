33f8cb95082755ab2a7c47cd4570a27668a77f45
showboatblastoff <124955423+showboatblastoff@users.noreply.github.com>
2024-12-15 22:30:47 -0600

    npm install

---
+++ next-react-project/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/showboat/locker/react-essential-training-5949338/next-react-project/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
